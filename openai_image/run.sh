fc82f01edb1c3af6ea6beaf43bd909921571485f
mythosaz <matt.stenger@gmail.com>
2026-01-04 14:07:20 -0700

    Initial commit - OpenAI Image Generator addon

---
+++ openai_image/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Starting OpenAI Image Generator add-on"
+
+# Pass options to Python script via environment
+export OPENAI_API_KEY="$(bashio::config 'openai_api_key')"
+export DEFAULT_MODEL="$(bashio::config 'default_model')"
+export DEFAULT_QUALITY="$(bashio::config 'default_quality')"
+export DEFAULT_SIZE="$(bashio::config 'default_size')"
+export OUTPUT_DIR="$(bashio::config 'output_dir')"
+
+# Run the generator (reads stdin forever)
+exec python3 /generator.py
